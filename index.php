b45b334705a9df6556d5747e3f4671fa445443d5
Jim Raynor <paolo@combi.li>
2019-01-08 22:06:21 +0100

    start working on new layout

---
+++ index.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,500,700"/>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/calendar.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+
+  
+
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  <div class="wrapper">
+    <main>
+      <div class="toolbar">
+        <div class="toggle">
+          <div class="toggle__option">week</div>
+          <div class="toggle__option toggle__option--selected">month</div>
+        </div>
+        <div class="current-month">June 2016</div>
+        <div class="search-input">
+          <input type="text" value="What are you looking for?">
+          <i class="fa fa-search"></i>
+        </div>
+      </div>
+      <div class="calendar">
+        <div class="calendar__header">
+          <div>mon</div>
+          <div>tue</div>
+          <div>wed</div>
+          <div>thu</div>
+          <div>fri</div>
+          <div>sat</div>
+          <div>sun</div>
+        </div>
+        <div class="calendar__week">
+          <div class="calendar__day day">1</div>
+          <div class="calendar__day day">2</div>
+          <div class="calendar__day day">3</div>
+          <div class="calendar__day day">4</div>
+          <div class="calendar__day day">5</div>
+          <div class="calendar__day day">6</div>
+          <div class="calendar__day day">7</div>
+        </div>
+        <div class="calendar__week">
+          <div class="calendar__day day">8</div>
+          <div class="calendar__day day">9</div>
+          <div class="calendar__day day">10</div>
+          <div class="calendar__day day">11</div>
+          <div class="calendar__day day">12</div>
+          <div class="calendar__day day">13</div>
+          <div class="calendar__day day">14</div>        
+        </div>
+        <div class="calendar__week">
+          <div class="calendar__day day">15</div>
+          <div class="calendar__day day">16</div>
+          <div class="calendar__day day">17</div>
+          <div class="calendar__day day">18</div>
+          <div class="calendar__day day">19</div>
+          <div class="calendar__day day">20</div>
+          <div class="calendar__day day">21</div>    
+        </div>
+        <div class="calendar__week">
+          <div class="calendar__day day">22</div>
+          <div class="calendar__day day">23</div>
+          <div class="calendar__day day">24</div>
+          <div class="calendar__day day">25</div>
+          <div class="calendar__day day">26</div> 
+          <div class="calendar__day day">27</div> 
+          <div class="calendar__day day">28</div> 
+        </div>
+        <div class="calendar__week">
+          <div class="calendar__day day">29</div>
+          <div class="calendar__day day">30</div>
+          <div class="calendar__day day">31</div>
+          <div class="calendar__day day">1</div>
+          <div class="calendar__day day">2</div>
+          <div class="calendar__day day">3</div>
+          <div class="calendar__day day">4</div>
+        </div>
+      </div>
+    </main>
+    <sidebar>
+      <div class="logo">logo</div>
+      <div class="avatar">
+        <div class="avatar__img">
+          <img src="https://picsum.photos/70" alt="avatar">
+        </div>
+        <div class="avatar__name">John Smith</div>
+      </div>
+      <nav class="menu">
+        <a class="menu__item" href="#">
+          <i class="menu__icon fa fa-home"></i>
+          <span class="menu__text">overview</span>
+        </a>
+        <a class="menu__item" href="#">
+          <i class="menu__icon fa fa-envelope"></i>
+          <span class="menu__text">messages</span>
+        </a>
+        <a class="menu__item" href="#">
+          <i class="menu__icon fa fa-list"></i>
+          <span class="menu__text">workout</span>
+        </a>
+        <a class="menu__item menu__item--active" href="#">
+          <i class="menu__icon fa fa-calendar"></i>
+          <span class="menu__text">calendar</span>
+        </a>
+        <a class="menu__item" href="#">
+          <i class="menu__icon fa fa-bar-chart"></i>
+          <span class="menu__text">goals</span>
+        </a>
+        <a class="menu__item" href="#">
+          <i class="menu__icon fa fa-trophy"></i>
+          <span class="menu__text">achivements</span>
+        </a>
+        <a class="menu__item" href="#">
+          <i class="menu__icon fa fa-sliders"></i>
+          <span class="menu__text">measurements</span>
+        </a>
+      </nav>
+      <div class="copyright">copyright &copy; 2018</div>
+    </sidebar>
+  </div>
+
+
+
+
+
+
+
+
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+
+  
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+
+</body>
+
+</html>
